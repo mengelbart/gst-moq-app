7a39aac9c0eaa9b6c2072d5724d41379f273b736
Mathis Engelbart <mathis.engelbart@gmail.com>
2023-10-07 13:10:56 +0200

    Implement basic media streaming server and client

--- go.mod
+++ go.mod
@@ -1,3 +1,28 @@
-module github.com/mengelbart/sfu
+module github.com/mengelbart/gst-moq-app
 
 go 1.21.1
+
+require (
+	github.com/mengelbart/gst-go v0.0.4
+	github.com/mengelbart/moqtransport v0.1.0
+)
+
+require (
+	github.com/go-task/slim-sprig v0.0.0-20230315185526-52ccab3ef572 // indirect
+	github.com/golang/mock v1.6.0 // indirect
+	github.com/google/pprof v0.0.0-20210407192527-94a9f03dee38 // indirect
+	github.com/onsi/ginkgo/v2 v2.9.5 // indirect
+	github.com/quic-go/qpack v0.4.0 // indirect
+	github.com/quic-go/qtls-go1-20 v0.3.3 // indirect
+	github.com/quic-go/quic-go v0.38.1 // indirect
+	github.com/quic-go/webtransport-go v0.5.3 // indirect
+	golang.org/x/crypto v0.4.0 // indirect
+	golang.org/x/exp v0.0.0-20221205204356-47842c84f3db // indirect
+	golang.org/x/mod v0.10.0 // indirect
+	golang.org/x/net v0.10.0 // indirect
+	golang.org/x/sys v0.8.0 // indirect
+	golang.org/x/text v0.9.0 // indirect
+	golang.org/x/tools v0.9.1 // indirect
+)
+
+replace github.com/mengelbart/moqtransport v0.1.0 => ../moqtransport
--- main.go
+++ main.go
@@ -1,4 +1,169 @@
 package main
 
+import (
+	"context"
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/tls"
+	"crypto/x509"
+	"encoding/pem"
+	"errors"
+	"flag"
+	"log"
+	"math/big"
+	"time"
+
+	"github.com/mengelbart/gst-go"
+	"github.com/mengelbart/moqtransport"
+)
+
 func main() {
+	isServer := flag.Bool("server", false, "run as receiving server")
+	flag.Parse()
+
+	gst.GstInit()
+	defer gst.GstDeinit()
+
+	if *isServer {
+		if err := server(); err != nil {
+			log.Fatal(err)
+		}
+		return
+	}
+	if err := client(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func server() error {
+	s := moqtransport.Server{
+		Handler: moqtransport.PeerHandlerFunc(func(p *moqtransport.Peer) {
+			log.Println("handling new peer")
+			defer log.Println("XXX")
+			p.OnAnnouncement(func(s string) error {
+				log.Printf("got announcement: %v", s)
+				return nil
+			})
+			if err := p.Announce("video"); err != nil {
+				log.Printf("failed to announce video: %v", err)
+			}
+			log.Println("announced video namespace")
+			p.OnSubscription(func(s string, st *moqtransport.SendTrack) (uint64, time.Duration, error) {
+				log.Printf("handling subscription to track %s", s)
+				if s != "video" {
+					return 0, 0, errors.New("unknown trackname")
+				}
+				p, err := gst.NewPipeline("videotestsrc ! queue ! videoconvert ! jpegenc ! multipartmux ! appsink name=appsink")
+				if err != nil {
+					log.Fatal(err)
+				}
+				p.SetBufferHandler(func(b gst.Buffer) {
+					if _, err := st.Write(b.Bytes); err != nil {
+						panic(err)
+					}
+				})
+				p.SetEOSHandler(func() {
+					p.Stop()
+				})
+				p.SetErrorHandler(func(err error) {
+					log.Println(err)
+					p.Stop()
+				})
+				p.Start()
+				return 0, 0, nil
+			})
+		}),
+		TLSConfig: generateTLSConfig(),
+	}
+	if err := s.ListenQUIC(context.Background(), "localhost:1909"); err != nil {
+		return err
+	}
+	return nil
+}
+
+func client() error {
+	announcementCh := make(chan string)
+	closeCh := make(chan struct{})
+
+	c, err := moqtransport.DialQUIC(context.Background(), "localhost:1909")
+	if err != nil {
+		return err
+	}
+	log.Println("moq peer connected")
+	c.OnAnnouncement(func(s string) error {
+		log.Printf("handling announcement of track %v", s)
+		announcementCh <- s
+		return nil
+	})
+
+	trackname := <-announcementCh
+	log.Printf("got announcement: %v", trackname)
+	p, err := gst.NewPipeline("appsrc name=src ! multipartdemux ! jpegdec ! autovideosink")
+	if err != nil {
+		return err
+	}
+	t, err := c.Subscribe(trackname)
+	if err != nil {
+		return err
+	}
+	p.SetEOSHandler(func() {
+		p.Stop()
+		closeCh <- struct{}{}
+	})
+	p.SetErrorHandler(func(err error) {
+		log.Println(err)
+		p.Stop()
+		closeCh <- struct{}{}
+	})
+	p.Start()
+	log.Println("starting pipeline")
+	go func() {
+		for {
+			log.Println("reading from track")
+			buf := make([]byte, 64_000)
+			n, err := t.Read(buf)
+			if err != nil {
+				log.Printf("error on read: %v", err)
+				p.SendEOS()
+			}
+			log.Printf("writing %v bytes from stream to pipeline", n)
+			_, err = p.Write(buf[:n])
+			if err != nil {
+				log.Printf("error on write: %v", err)
+				p.SendEOS()
+			}
+		}
+	}()
+
+	ml := gst.NewMainLoop()
+	go func() {
+		<-closeCh
+		ml.Stop()
+	}()
+	ml.Run()
+	return nil
+}
+
+// Setup a bare-bones TLS config for the server
+func generateTLSConfig() *tls.Config {
+	key, err := rsa.GenerateKey(rand.Reader, 1024)
+	if err != nil {
+		panic(err)
+	}
+	template := x509.Certificate{SerialNumber: big.NewInt(1)}
+	certDER, err := x509.CreateCertificate(rand.Reader, &template, &template, &key.PublicKey, key)
+	if err != nil {
+		panic(err)
+	}
+	keyPEM := pem.EncodeToMemory(&pem.Block{Type: "RSA PRIVATE KEY", Bytes: x509.MarshalPKCS1PrivateKey(key)})
+	certPEM := pem.EncodeToMemory(&pem.Block{Type: "CERTIFICATE", Bytes: certDER})
+
+	tlsCert, err := tls.X509KeyPair(certPEM, keyPEM)
+	if err != nil {
+		panic(err)
+	}
+	return &tls.Config{
+		Certificates: []tls.Certificate{tlsCert},
+		NextProtos:   []string{"moq-00"},
+	}
 }
